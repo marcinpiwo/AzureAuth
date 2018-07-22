413f2ad22a9b46cf3d94a12ce2ba32427091e569
Marcin Piwowarski <marcpiwowarski@hotmail.com>
2018-07-22 23:17:35 +0100

    Add project files.

---
+++ AzureAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
